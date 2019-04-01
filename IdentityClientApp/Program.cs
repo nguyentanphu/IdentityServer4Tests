8cce385b4090e48dd0fb59ffc797b5fb74fbfe96
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2019-04-01 16:20:55 +0700

    Add project files.

---
+++ IdentityClientApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityClientApp
+{
+	internal class Program
+	{
+		private static async Task Main(string[] args)
+		{
+			// discover endpoints from metadata
+			var client = new HttpClient();
+			var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+			if (disco.IsError)
+			{
+				Console.WriteLine(disco.Error);
+				return;
+			}
+
+			// request token
+			var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+			{
+				Address = disco.TokenEndpoint,
+				ClientId = "client.password",
+				ClientSecret = "secret",
+
+				UserName = "alice",
+				Password = "password",
+				Scope = "api1"
+			});
+
+			if (tokenResponse.IsError)
+			{
+				Console.WriteLine(tokenResponse.Error);
+				return;
+			}
+
+			Console.WriteLine(tokenResponse.Json);
+		}
+	}
+}
